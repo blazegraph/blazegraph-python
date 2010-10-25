e8219a17e2a65262aacf19c271807a73e2d4b1e1
Nick Pilon <npilon@gmail.com>
2010-10-24 22:01:32 -0700

    Begin overhaul for new API. Auto-instantiation of resources.

--- pymantic/RDF.py
+++ pymantic/RDF.py
@@ -27,6 +27,15 @@ class SaneURIRef(Original_URIRef):
 
 rdflib.URIRef = SaneURIRef # Monkey patch!
 
+def is_language(lang):
+    """Is something a valid XML language?"""
+    return True
+
+def lang_match(lang1, lang2):
+    if lang1 is None or lang2 is None:
+        return True
+    return lang1 == lang2
+
 def parse_curie(curie, namespaces):
     """
     Parses a CURIE within the context of the given namespaces. Will also accept
@@ -82,6 +91,9 @@ class MetaResource(type):
     MetaResource makes namespaces a special attribute. namespaces is a
     dictionary that is the 'union' of all namespaces attributes on a
     class and its parents."""
+    
+    _classes = {} # Map of RDF classes to Python classes.
+    
     def __new__(cls, name, bases, dct):
         namespaces = {}
         for base in bases:
@@ -94,209 +106,16 @@ class MetaResource(type):
         dct['namespaces'] = namespaces
         return type.__new__(cls, name, bases, dct)
 
-class ClassificationMismatchError(Exception):
-    """Raised when an attempt is made to create a Subject for a resource that
-    does not match its classification constraints."""
-    pass
+def register_class(rdf_type):
+    def _register_class(python_class):
+        MetaResource._classes[python_class.resolve(rdf_type)] = python_class
+        return python_class
+    return _register_class
 
 class URLRetrievalError(Exception):
     """Raised when an attempt to retrieve a resource returns a status other
     than 200 OK."""
     pass
-
-
-class PredicateProperty(property):
-    """
-    Provides property-style access to the objects related to a Subject
-    by a given predicate. If a language is specififed only values in that
-    language will be returned.
-    """
-
-    def __init__(self, predicate, docstring=None, language=None):
-        self.predicate = predicate
-        self.language = language
-        self.__doc__ = ("""Returns list of values for predicate :term:`%s`""" 
-                        % (predicate,)) + (docstring or "")
-    
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-        if self.language:
-            value = [value for value in instance[self.predicate] 
-                     if value.language == self.language]
-        else:
-            value = list(instance[self.predicate])
-        return value
-
-class ScalarPredicateProperty(PredicateProperty):
-    """
-    Provides property-style access to a predicate that can only have one value.
-    If a language is specififed only values in that language will be returned.
-    
-    None is returned if there are no values, and ValueError raised if
-    there are multiples.
-    """
-
-    def __init__(self, predicate, docstring=None, language=None):
-        self.predicate = predicate
-        self.language = language
-        self.__doc__ = ("""Returns a value for predicate :term:`%s`
-
-""" % (predicate,)) + (docstring or "")
-
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-        value = util.one_or_none(
-            super(ScalarPredicateProperty, self).__get__(instance, owner))
-        return value
-
-class ResourcePredicateProperty(property):
-    """
-    Provides property-style access to a predicate whose object is another
-    resource. These yeild further Resources.
-    
-    cls is the class to be used to represent the resulting Resources. The
-    Resources are provided as a generator. Any Resources that do not meet the
-    classification constraints of cls (and, thus, raise a
-    ClassificationMismatchError on instantiation) are silently omitted from the
-    resulting generator.
-    """
-    def __init__(self, predicate, cls, omit=True, docstring=None):
-        self.omit = omit
-        self.predicate = predicate
-        self.cls = cls
-        self.__doc__ = ("""Returns a list of values for predicate :term:`%s` as :class:`~%s.%s`""" 
-                        % (predicate,cls.__module__,cls.__name__)) + (docstring or "")
-    
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-        else:
-            return list(self._resources(instance, owner))
-    
-    def _resources(self, instance, owner):
-        resource_generator = instance[self.predicate]
-        for resource in resource_generator:
-            seq = instance.graph.seq(resource)
-            if seq:
-                for seq_res in seq:
-                    try:
-                        yield self.cls(instance.graph, seq_res)
-                    except ClassificationMismatchError:
-                        if self.omit:
-                            continue
-                        else:
-                            yield seq_res
-            else:
-                try:
-                    yield self.cls(instance.graph, resource)
-                except ClassificationMismatchError:
-                    if self.omit:
-                        continue
-                    else:
-                        yield seq_res
-
-class ScalarResourcePredicateProperty(ResourcePredicateProperty):
-    """
-    Provides property-style access to a Resource predicate that can
-    yield only one resource.
-    """
-    def __init__(self, predicate, cls, docstring=None):
-        super(ScalarResourcePredicateProperty, self).__init__(predicate, cls)
-        self.__doc__ = ("""Returns a value for predicate :term:`%s` as :class:`~%s.%s`""" % (predicate,cls.__module__,cls.__name__)) + (docstring or "")
-
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-        value = util.one_or_none(list(
-            super(ScalarResourcePredicateProperty, self).__get__(instance, owner)))
-        return value
-
-class ResourceReversePredicateProperty(property):
-    """
-    Provides property-style access to a Resource predicate that yields the
-    Resource by following the predicate backwards, using the current subject
-    as the object of the graph query.
-    """
-    def __init__(self, predicate, cls, docstring=None):
-        self.predicate = predicate
-        self.cls = cls
-        self.__doc__ = ("""Returns list of subjects with predicate :term:`%s` as a :class:`~%s.%s` where the predicate is the current object""" % (predicate,cls.__module__,cls.__name__)) + (docstring or "")
-    
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-        else:
-            return list(self._resources(instance, owner))
-    
-    def _resources(self, instance, owner):
-        resource_generator = instance.graph.subjects(
-            instance.resolve(self.predicate), instance.subject)
-        for resource in resource_generator:
-            try:
-                yield self.cls(instance.graph, resource)
-            except ClassificationMismatchError:
-                continue
-
-class SelfReferentialPredicateProperty(property):
-    """
-    Provides property-style access to a Resource predicate that yields the same
-    type of things as the predicate is on.
-    """
-    def __init__(self, predicate, docstring=None):
-        self.predicate = predicate
-        self.__doc__ = docstring
-    
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-        else:
-            return self._resources(instance, owner)
-    
-    def _resources(self, instance, owner):
-        resource_generator = instance[self.predicate]
-        for resource in resource_generator:
-            seq = instance.graph.seq(resource)
-            if seq:
-                for seq_res in seq:
-                    try:
-                        yield type(instance)(instance.graph, seq_res)
-                    except ClassificationMismatchError:
-                        continue
-            else:
-                try:
-                    yield type(instance)(instance.graph, resource)
-                except ClassificationMismatchError:
-                    continue
-                
-class ScalarSelfReferentialPredicateProperty(SelfReferentialPredicateProperty):
-    """
-    Provides property-style access to a Resource predicate that yields the same
-    type of things as the predicate is on.
-    """
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-        value = util.one_or_none(list(
-            super(ScalarSelfReferentialPredicateProperty, self).__get__(instance, owner)))
-        return value
-
-class InverseFunctionalLookupProperty(property):
-    """Look up and return instances of another resource based on the values of a
-    predicate on this resource using an Inverse Functional Property.
-    
-    lookup_method is expected to take (graph, value) and return an iteratable
-    of results."""
-    
-    def __init__(self, predicate, lookup_method):
-        self.predicate = predicate
-        self.lookup_method = lookup_method
-    
-    def __get__(self, instance, owner):
-        for value in instance[self.predicate]:
-            for result in self.lookup_method(instance.graph, value):
-                yield result
 
 class Resource(object):
     """
@@ -339,10 +158,6 @@ class Resource(object):
                   'rdfs': 'http://www.w3.org/2000/01/rdf-schema#'}
     
     classification_predicate = 'rdf:type'
-    
-    label = ScalarPredicateProperty('rdfs:label')
-    
-    rdf_type = PredicateProperty('rdf:type')
     
     def __init__(self, graph, subject):
         self.graph = graph
@@ -408,33 +223,7 @@ class Resource(object):
                 return True
         else:
             return True
-    
-    @classmethod
-    def for_uri(cls, graph, identifier):
-        """Basically the same as __init__, but handles wrapping identifier in
-        a rdflib.URIRef if necessary and handles type errors more gracefully."""
-        try:
-            possible = cls(graph, identifier
-                                  if isinstance(identifier, rdflib.URIRef)
-                                  else rdflib.URIRef(identifier))
-            return possible
-        except ClassificationMismatchError:
-            return None
-    
-    @classmethod
-    def in_graph(cls, graph):
-        """Iterates over all the instances of this Resource found in the
-        specified graph. This requires that the Resource have a RDF_type
-        specified."""
-        if not cls.classification_value:
-            raise Exception()
-        for subj in graph.subjects(cls.resolve(cls.classification_predicate),
-                                   cls.resolve(cls.classification_value)):
-            try:
-                yield cls(graph, subj)
-            except ClassificationMismatchError:
-                continue
-    
+        
     @classmethod
     def resolve(cls, key):
         """Use the classes namespaces to resolve a curie"""
@@ -456,7 +245,26 @@ class Resource(object):
     
     def __getitem__(self, key):
         """Fetch predicates off this subject by key dictionary-style."""
-        return self.objects(self.resolve(key))
+        lang = None
+        datatype = None
+        if isinstance(key, tuple) and len(key) >= 2:
+            if is_language(key[1]):
+                lang = key[1]
+            else:
+                datatype = key[1]
+        if lang is None and datatype is None:
+            lang = self.lang
+        predicate = self.resolve(key)
+        objects = [obj for obj in self.graph.objects(self.subject, predicate) if\
+                   (hasattr(obj, 'lang') and lang_match(lang, obj.lang)) or
+                   not hasattr(obj, 'lang')]
+        if predicate in self._scalars:
+            return self._possibly_instantiate(self.graph, util.one_or_none(objects))
+        else:
+            def getitem_iter_results():
+                for obj in objects:
+                    yield self._possibly_instantiate(self.graph, obj)
+            return getitem_iter_results()
     
     def __repr__(self):
         return "<%r: %r %r>" % (type(self), self.graph, self.subject)
@@ -467,13 +275,22 @@ class Resource(object):
         else:
             return self.subject
     
-    def objects(self, predicate):
-        """Fetch the objects for a predicate on this subject."""
-        return self.graph.objects(self.subject, predicate)
-    
-    def predicate_objects(self):
-        """Fetch all predicate, object pairs for this subject."""
-        return self.graph.predicate_objects(self.subject)
+    @classmethod
+    def _possibly_instantiate(cls, graph, obj):
+        if isinstance(obj, rdflib.Literal):
+            return obj
+        types = tuple(sorted(graph.objects(obj, cls.resolve('rdf:type'))))
+        python_classes = tuple(self.__metaclass__._classes[t] for t in types)
+        if len(python_classes) == 1:
+            return python_classes[0](graph, obj)
+        else:
+            if types not in self.__metaclass__._classes:
+                the_class = self.__metaclass__.__new__(
+                    self.__metaclass__, ''.join(python_class.__name__ for\
+                                                python_class in python_classes),
+                    python_classes, {'_autocreate': True})
+                self.__metaclass__._classes[types] = the_class
+            return self.__metaclass__._classes[types]
 
 class Property(Resource):
     """A rdf:Property."""
