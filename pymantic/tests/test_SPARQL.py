4ae4bf516c0145ff5e86ed35f309678c4f8d9178
Nick Pilon <npilon@gmail.com>
2010-11-06 13:53:14 -0700

    SPARQL tests pass again.

--- pymantic/tests/test_SPARQL.py
+++ pymantic/tests/test_SPARQL.py
@@ -8,21 +8,24 @@ from pymantic.SPARQL import SPARQLServer, SPARQLQueryException
 class TestSparql(unittest.TestCase):
     def setUp(self):
         self.mock_port = 48558
+        self.mock_endpoint = mock_http.MockHTTP(self.mock_port)
     
     def testMockSPARQL(self):
         """Test a SPARQL query against a mocked-up endpoint."""
         test_query = """PREFIX dc: <http://purl.org/dc/terms/>
         SELECT ?product ?title WHERE { ?product dc:title ?title } LIMIT 10"""
         test_json = '{"head": {"vars": ["product", "title"]}, "results": {"bindings": [{"product": {"type": "uri", "value": "test_product"}, "title": {"xml:lang": "en", "type": "literal", "value": "Test Title"}}]}}'
-        mock_endpoint = mock_http.MockHTTP(self.mock_port)
-        mock_endpoint.expects(
+        
+        self.mock_endpoint.expects(
             method='POST', times=mock_http.once, path='/tenuki/sparql',
             headers={'Content-Type': 'application/x-www-form-urlencoded',},
-            body=urllib.urlencode({'query': test_query, 'output': 'json',})).will(
+            params={'query': test_query, 'output': 'json',}).will(
                 body=test_json, headers={'content-type': 'application/sparql-results+json'})
         sparql = SPARQLServer('http://localhost:%d/tenuki/sparql' % self.mock_port)
-        results = sparql.query(test_query)
-        mock_endpoint.verify()
+        try:
+            results = sparql.query(test_query)
+        finally:
+            self.assert_(self.mock_endpoint.verify())
         self.assertEqual(results['results']['bindings'][0]['product']['value'],
                          'test_product')
         self.assertEqual(results['results']['bindings'][0]['title']['value'],
@@ -32,12 +35,13 @@ class TestSparql(unittest.TestCase):
         """Test a SPARQL query against a mocked-up endpoint."""
         test_query = """PREFIX dc: <http://purl.org/dc/terms/>
         SELECT ?product ?title WHERE { ?product dc:title ?title } LIMIT 10"""
-        mock_endpoint = mock_http.MockHTTP(self.mock_port)
-        mock_endpoint.expects(
+        self.mock_endpoint.expects(
             method='POST', times=mock_http.once, path='/tenuki/sparql',
             headers={'Content-Type': 'application/x-www-form-urlencoded',},
-            body=urllib.urlencode({'query': test_query, 'output': 'json',})).will(
+            params={'query': test_query, 'output': 'json',}).will(
                 http_code=500)
         sparql = SPARQLServer('http://localhost:%d/tenuki/sparql' % self.mock_port)
-        self.assertRaises(SPARQLQueryException, sparql.query, test_query)
-        mock_endpoint.verify()
+        try:
+            self.assertRaises(SPARQLQueryException, sparql.query, test_query)
+        finally:
+            self.assert_(self.mock_endpoint.verify())
