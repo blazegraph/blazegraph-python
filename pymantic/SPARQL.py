a5256027378798a13e599b6e20fd413c6108bc67
Nick Pilon <npilon@gmail.com>
2010-11-07 20:55:46 -0800

    Start of infrastructure for CRUD operation.

--- pymantic/SPARQL.py
+++ pymantic/SPARQL.py
@@ -58,3 +58,74 @@ class SPARQLServer(object):
         else:
             raise UnknownSPARQLReturnTypeException('Got content of type: %s' %\
                                                    response['content-type'])
+
+class UpdateableGraphStore(SPARQLServer):
+    """SPARQL server class that is capable of interacting with SPARQL 1.1
+    graph stores."""
+    
+    def __init__(self, query_url, dataset_url):
+        super(UpdateableRDFStore, self).__init__(query_url)
+        self.dataset_url = dataset_url
+    
+    def request_url(self, graph_uri):
+        return urllib.urlencode({'graph', graph_uri})
+    
+    def get(self, graph_uri):
+        h = httplib2.Http()
+        resp, content = h.request(uri = self.request_url(graph_uri), method = 'GET')
+        if resp['status'] != '200':
+            raise Exception('Error from Graph Store (%s): %s' %\
+                            (resp['status'], content))
+        graph = rdflib.ConjunctiveGraph()
+        if resp['content-type'].startswith('application/rdf+xml'):
+            graph.parse(StringIO(content), publicID=graph_uri, format='xml')
+        elif resp['content-type'].startswith('text/turtle'):
+            graph.parse(StringIO(content), publicID=graph_uri, format='turtle')
+        elif resp['content-type'].startswith('text/rdf+n3'):
+            graph.parse(StringIO(content), publicID=graph_uri, format='n3')
+        elif resp['content-type'].startswith('text/plain'):
+            graph.parse(StringIO(content), publiCID=graph_uri, format='nt')
+    
+    def delete(self, graph_uri):
+        h = httplib2.Http()
+        resp, content = h.request(uri = self.request_url(graph_uri),
+                                  method = 'DELETE')
+        if resp['status'] != '200' and resp['status'] != '202':
+            raise Exception('Error from Graph Store (%s): %s' %\
+                            (resp['status'], content))
+    
+    def put(self, graph_uri, graph):
+        h = httplib2.Http()
+        graph_triples = graph.serialize(format = 'nt')
+        resp, content = h.request(uri = self.request_url(graph_uri),
+                                  method = 'PUT', body = graph_triples,
+                                  headers = {'content-type': 'text/plain',},)
+        if resp['status'] not in ('200', '201', '204'):
+            raise Exception('Error from Graph Store (%s): %s' %\
+                            (resp['status'], content))
+    
+    def post(self, graph_uri, graph):
+        h = httplib2.Http()
+        graph_triples = graph.serialize(format = 'nt')
+        if graph_uri != None:
+            resp, content = h.request(uri = self.request_url(graph_uri),
+                                      method = 'POST', body = graph_triples,
+                                      headers = {'content-type': 'text/plain',},)
+            if resp['status'] not in ('200', '201', '204'):
+                raise Exception('Error from Graph Store (%s): %s' %\
+                                (resp['status'], content))
+        else:
+            resp, content = h.request(uri = self.dataset_url, method = 'POST',
+                                      body = graph_triples,
+                                      headers = {'content-type': 'text/plain',},)
+            if resp['status'] != '201':
+                raise Exception('Error from Graph Store (%s): %s' %\
+                                (resp['status'], content))
+
+class PatchableGraphStore(SPARQLServer):
+    """A graph store that supports the optional PATCH method of updating
+    RDF graphs."""
+    
+    def patch(self, graph_uri, changeset):
+        h = httplib2.Http()
+        
