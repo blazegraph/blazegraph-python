ed1f2f8c89b0d29829d588034a45064417579077
Nick Pilon <npilon@oreilly.com>
2010-11-29 14:22:20 -0800

    Add NQuads parser.

--- pymantic/__init__.py
+++ pymantic/__init__.py
@@ -1,6 +1,8 @@
 #
 from rdflib.plugin import register
 from rdflib.serializer import Serializer
+from rdflib.parser import Parser
 
 register('nt', Serializer, 'pymantic.serializers', 'NTSerializer')
 register('nq', Serializer, 'pymantic.serializers', 'NQSerializer')
+register('nq', Parser, 'pymantic.parsers', 'NQParser')
