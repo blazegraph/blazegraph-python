b704954b3e0fe9dc7f8fbc474af763ae8eb115fb
Nick Pilon <npilon@oreilly.com>
2010-12-01 10:03:39 -0800

    Dictionary for mapping between rdflib format names and standard content types.

--- pymantic/__init__.py
+++ pymantic/__init__.py
@@ -6,3 +6,13 @@ from rdflib.parser import Parser
 register('nt', Serializer, 'pymantic.serializers', 'NTSerializer')
 register('nq', Serializer, 'pymantic.serializers', 'NQSerializer')
 register('nq', Parser, 'pymantic.parsers', 'NQParser')
+
+content_type_to_rdflib_format = {
+    'text/plain': 'nt',
+    'text/x-nquads': 'nq',
+    'application/rdf+xml': 'xml',
+    'text/turtle': 'turtle',
+}
+
+rdflib_format_to_content_type = dict((value, key) for key, value in\
+                                     content_type_to_rdflib_format.iteritems())
