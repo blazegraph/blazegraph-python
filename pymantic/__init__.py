76e8001a79f67fa23c979e846cbc86a9c30ddd39
Nick Pilon <npilon@oreilly.com>
2010-12-04 13:32:50 -0800

    NTriple parser patch.

--- pymantic/__init__.py
+++ pymantic/__init__.py
@@ -2,6 +2,12 @@
 from rdflib.plugin import register
 from rdflib.serializer import Serializer
 from rdflib.parser import Parser
+import re
+
+# Fix rdflib's ntriples parser
+from rdflib.plugins.parsers import ntriples
+ntriples.litinfo = r'(?:@([a-z]+(?:-[a-zA-Z0-9]+)*)|\^\^' + ntriples.uriref + r')?'
+ntriples.r_literal = re.compile(ntriples.literal + ntriples.litinfo)
 
 register('nt', Serializer, 'pymantic.serializers', 'NTSerializer')
 register('nq', Serializer, 'pymantic.serializers', 'NQSerializer')
