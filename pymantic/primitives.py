dc49b5902b2081d0c9d2ed72b82679519b6ec9a3
Gavin Carothers <gavin@carothers.name>
2010-12-23 14:11:51 -0800

    Intial primitives

---
+++ pymantic/primitives.py
@@ -0,0 +1,44 @@
+import collections
+from operator import itemgetter
+
+Triple = collections.namedtuple('Triple', 'subject predicate object')
+Quad = collections.namedtuple('Quad', 'graph subject predicate object')
+
+class Literal(tuple):
+    """Literal(value, language, datatype)""" 
+
+    __slots__ = () 
+
+    _fields = ('value', 'language', 'datatype') 
+
+    def __new__(_cls, value, language=None, datatype=None):
+        return tuple.__new__(_cls, (value, language, datatype)) 
+
+    @classmethod
+    def _make(cls, iterable, new=tuple.__new__, len=len):
+        'Make a new Literal object from a sequence or iterable'
+        result = new(cls, iterable)
+        if len(result) != 3:
+            raise TypeError('Expected 3 arguments, got %d' % len(result))
+        return result 
+
+    def __repr__(self):
+        return 'Literal(value=%r, language=%r, datatype=%r)' % self 
+
+    def _asdict(t):
+        'Return a new dict which maps field names to their values'
+        return {'value': t[0], 'language': t[1], 'datatype': t[2]} 
+
+    def _replace(_self, **kwds):                                                                            
+        'Return a new Literal object replacing specified fields with new values'
+        result = _self._make(map(kwds.pop, ('value', 'language', 'datatype'), _self))
+        if kwds:
+            raise ValueError('Got unexpected field names: %r' % kwds.keys())
+        return result 
+
+    def __getnewargs__(self):
+        return tuple(self) 
+
+    value = property(itemgetter(0))
+    language = property(itemgetter(1))
+    datatype = property(itemgetter(2))
