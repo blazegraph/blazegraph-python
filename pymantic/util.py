f562aff86a4bb0e081593db5696f1589e167d4a1
Gavin Carothers <gavin@carothers.name>
2010-12-23 14:00:14 -0800

    Clean up some documentation

--- pymantic/util.py
+++ pymantic/util.py
@@ -3,8 +3,13 @@
 import rdflib
 
 def en(value):
-    """Returns an RDF literal from the en-US language for the given value."""
+    """Returns an RDF literal from the en language for the given value."""
     return rdflib.Literal(value, lang='en')
+
+def de(value):
+    """Returns an RDF literal from the de language for the given value."""
+    return rdflib.Literal(value, lang='de')
+
 
 def one_or_none(values):
     """Fetch the first value from values, or None if values is empty. Raises
