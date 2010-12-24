30d9b869c7fa8fbe0fe445bcbb3b89f656bf12fc
Gavin Carothers <gavin@carothers.name>
2010-12-23 16:32:17 -0800

    Some not very good scale testing

--- pymantic/tests/test_primitives.py
+++ pymantic/tests/test_primitives.py
@@ -1,5 +1,6 @@
 from nose.tools import *
 from pymantic.primitives import *
+import random
 
 def test_simple_add():
     t = Triple("http://example.com", "http://purl.org/dc/terms/issued","Never!")
@@ -55,3 +56,17 @@ def test_match_VpV_pattern():
     g.add(t)
     matches = g.match(Triple(None, "http://purl.org/dc/terms/issued", None))
     assert t in matches
+    
+def generate_triples(n=10):
+    for i in range(1,n):
+        yield Triple("http://example/" + str(random.randint(1,1000)),
+                   "http://example/terms/" + str(random.randint(1,1000)),
+                   random.randint(1,1000))
+
+def test_1000_triples():
+    g = TripleGraph()
+    for t in generate_triples(1000):
+        g.add(t)
+    matches = g.match(Triple("http://example.com/42", None, None))
+    matches = g.match(Triple(None, "http://example/terms/42", None))
+    matches = g.match(Triple(None, None, 42))
