5e0f931ae876c9c8f31ff2e6a5188de6e54aeea7
Nick Pilon <npilon@gmail.com>
2010-12-28 20:01:43 -0800

    Start of lepl ntriples parser.

--- pymantic/parsers.py
+++ pymantic/parsers.py
@@ -60,3 +60,56 @@ class NQuadsParser(NTriplesParser):
         if not graph:
             raise ParseError('Graph must be a uriref')
         return graph
+
+from lepl import *
+
+def parse_ntriples(stream, graph):
+    bnodes = {}
+    
+    def make_datatype_literal(values):
+        from pymantic.primitives import Literal
+        return Literal(value = values[0], datatype = values[1])
+    
+    def make_language_literal(values):
+        from pymantic.primitives import Literal
+        if len(values) == 2:
+            return Literal(value = values[0], language = values[1])
+        else:
+            return Literal(value = values[0])
+    
+    def make_uri(values):
+        from pymantic.primitives import URI
+        return URI(values[0])
+    
+    def make_bnode(values):
+        from pymantic.primitives import BNode
+        if values[0] not in bnodes:
+            bnodes[values[0]] = BNode()
+        return bnodes[values[0]]
+    
+    def make_triple(values):
+        from pymantic.primitives import Triple
+        triple = Triple(*values)
+        graph.add(triple)
+        return triple
+
+    string = Regexp(r'[^"\\]*(?:\\.[^"\\]*)*')
+    name = Regexp(r'[A-Za-z][A-Za-z0-9]*')
+    absoluteURI = Regexp(r'[^:]+:[^\s"<>]+')
+    language = Regexp(r'[a-z]+(?:-[a-z0-9]+)*')
+    uriref = ~Literal('<') & absoluteURI & ~Literal('>') > make_uri
+    datatypeString = ~Literal('"') & string & ~Literal('"') & ~Literal('^^') & uriref > make_datatype_literal
+    langString = ~Literal('"') & string & ~Literal('"') & Optional(~Literal('@') & language) > make_language_literal
+    literal = datatypeString | langString
+    nodeID = ~Literal('_:') & name > make_bnode
+    object_ = uriref | nodeID | literal
+    predicate = uriref
+    subject = uriref | nodeID
+    triple = subject & ~Plus(Space()) & predicate & ~Plus(Space()) & object_ & ~Star(Space()) & ~Literal('.') & ~Star(Space()) >= make_triple
+    comment = Literal('#') & Regexp(r'[ -~]*')
+    line = Star(Space()) & Optional(~triple | ~comment) & ~Literal('\n')
+    document = Star(line)
+    
+    document.parse_file(stream)
+    
+    return graph
