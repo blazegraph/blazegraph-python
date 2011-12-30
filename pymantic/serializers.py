7ccc032a82ab0f7ef84a34f2e9b97aadfa1186ed
Nicholas Pilon <npilon@lexmachina.com>
2011-12-29 16:46:43 -0800

    Work on serializers, tests for basic nodes.

--- pymantic/serializers.py
+++ pymantic/serializers.py
@@ -33,55 +33,81 @@ def serialize_nquads(dataset, f):
     for quad in dataset:
         f.write(str(quad))
 
-# def default_bnode_name_generator():
-#     i = 0
-#     while True:
-#         yield '_b' + str(i)
-#         i += 1
+def default_bnode_name_generator():
+    i = 0
+    while True:
+        yield '_b' + str(i)
+        i += 1
 
-# def escape_prefix_local(prefix):
-#     prefix, colon, local = prefix.partition(':')
-#     for esc_char in "~.-!$&'()*+,;=:/?#@%_":
-#         local = local.replace(esc_char, '\\' + esc_char)
-#     return ''.join((prefix,colon,local))
+def escape_prefix_local(prefix):
+    prefix, colon, local = prefix.partition(':')
+    for esc_char in "~.-!$&'()*+,;=:/?#@%_":
+        local = local.replace(esc_char, '\\' + esc_char)
+    return ''.join((prefix,colon,local))
 
-# def turtle_name(node, profile, name_map, bnode_name_maker):
-#     if isinstance(node, primitives.NamedNode):
-#         name = profile.prefixes.shrink(node)
-#         if name == node:
-#             name = '<' + node + '>'
-#         else:
-#             escape_prefix_local(name)
-#     elif isinstance(node, primitives.BlankNode):
-#         if node in name_map:
-#             name = name_map[node]
-#         else:
-#             name = bnode_name_maker.next()
-#             name_map[node] = name
-#     elif isinstance(node, primitives.Literal):
-#         if node.datatype is not None:
-#             name = turtle_string_escape(node.value) + '^'
-#             name += turtle_name(node.datatype, profile, None, None)
-#     return name
+def turtle_string_escape(string):
+    from pymantic.parsers import TurtleParser
 
-# def serialize_turtle(graph, f, base=None, profile=Profile(),
-#                      bnode_name_generator=default_bnode_name_generator):
-#     """Serialize some turtle from a graph to f, optionally using base IRI base
-#     and prefix map from profile. If provided, subject_key will be used to order
-#     subjects, and predicate_key predicates within a subject."""
+    for escaped, value in TurtleParser.echar_map.iteritems():
+        string = string.replace(value, '\\' + escaped)
+    return '"' + string + '"'
 
-#     if base is not None:
-#         f.write('@base <' + base + '> .\n')
-#     for prefix, iri in profile.prefixes:
-#         f.write('@prefix ' + prefix + ': <' + iri + '>\n')
+def turtle_representation(node, profile, name_map, bnode_name_maker):
+    """Turn a node in an RDF graph into its turtle representation."""
+    if node.interfaceName == 'NamedNode':
+        name = profile.prefixes.shrink(node)
+        if name == node:
+            name = '<' + unicode(node) + '>'
+        else:
+            escape_prefix_local(name)
+    elif node.interfaceName == 'BlankNode':
+        if node in name_map:
+            name = name_map[node]
+        else:
+            name = bnode_name_maker.next()
+            name_map[node] = name
+    elif node.interfaceName == 'Literal':
+        if node.datatype == profile.resolve('xsd:string'):
+            # Simple string.
+            name = turtle_string_escape(node.value)
+        elif node.datatype == None:
+            # String with language.
+            name = turtle_string_escape(node.value)
+            name += '@' + node.language
+        elif node.datatype == profile.resolve('xsd:integer'):
+            name = node.value
+        elif node.datatype == profile.resolve('xsd:decimal'):
+            name = node.value
+        elif node.datatype == profile.resolve('xsd:double'):
+            name = node.value
+        elif node.datatype == profile.resolve('xsd:boolean'):
+            name = node.value
+        else:
+            # Unrecognized data-type.
+            name = turtle_string_escape(node.value)
+            name += '^' + turtle_representation(node.datatype, profile, None, None)
+    return name
+
+def serialize_turtle(graph, f, base=None, profile=None,
+                     bnode_name_generator=default_bnode_name_generator):
+    """Serialize some turtle from a graph to f, optionally using base IRI base
+    and prefix map from profile. If provided, subject_key will be used to order
+    subjects, and predicate_key predicates within a subject."""
+
+    if base is not None:
+        f.write('@base <' + base + '> .\n')
+    if profile is None:
+        from pymantic.primitives import Profile
+        profile = Profile()
+    for prefix, iri in profile.prefixes:
+        f.write('@prefix ' + prefix + ': <' + iri + '>\n')
     
-#     name_map = {}
-#     bnode_name_maker = bnode_name_generator()
+    name_map = {}
+    bnode_name_maker = bnode_name_generator()
     
-#     for subject in sorted(graph.subjects()):
-#         subject_name = turtle_name(subject, profile, name_map, bnode_name_maker)
-#         indent_size = len(subject_name) + 1
-#         for triple in sorted(graph.match(subject=name_map[subject]),
-#                              key=lambda t: predicate_key(t.predicate)):
-            
-
+    for subject in sorted(graph.subjects()):
+        subject_name = turtle_name(subject, profile, name_map, bnode_name_maker)
+        indent_size = len(subject_name) + 1
+        for triple in sorted(graph.match(subject=name_map[subject]),
+                             key=lambda t: predicate_key(t.predicate)):
+            pass
--- pymantic/tests/test_serializers.py
+++ pymantic/tests/test_serializers.py
@@ -1,3 +1,5 @@
+from unittest import TestCase
+
 from cStringIO import StringIO
 from nose.tools import *
 from pymantic.parsers import *
@@ -22,3 +24,78 @@ def test_parse_ntriples_named_nodes():
     assert Triple(NamedNode('http://example.com/objects/2'),
                   NamedNode('http://example.com/predicates/2'),
                   NamedNode('http://example.com/objects/1')) in g2
+
+class TestTurtleRepresentation(TestCase):
+    def setUp(self):
+        from pymantic.serializers import turtle_representation
+        self.turtle_representation = turtle_representation
+        import pymantic.primitives
+        self.primitives = pymantic.primitives
+        self.profile = self.primitives.Profile()
+    
+    def test_integer(self):
+        lit = self.primitives.Literal(value='42', datatype=self.profile.resolve('xsd:integer'))
+        name = self.turtle_representation(node = lit, profile = self.profile, name_map = None, bnode_name_maker = None)
+        self.assertEqual(name, '42')
+    
+    def test_decimal(self):
+        lit = self.primitives.Literal(value='4.2', datatype=self.profile.resolve('xsd:decimal'))
+        name = self.turtle_representation(node = lit, profile = self.profile, name_map = None, bnode_name_maker = None)
+        self.assertEqual(name, '4.2')
+    
+    def test_double(self):
+        lit = self.primitives.Literal(value='4.2e1', datatype=self.profile.resolve('xsd:double'))
+        name = self.turtle_representation(node = lit, profile = self.profile, name_map = None, bnode_name_maker = None)
+        self.assertEqual(name, '4.2e1')
+    
+    def test_boolean(self):
+        lit = self.primitives.Literal(value='true', datatype=self.profile.resolve('xsd:boolean'))
+        name = self.turtle_representation(node = lit, profile = self.profile, name_map = None, bnode_name_maker = None)
+        self.assertEqual(name, 'true')
+    
+    def test_bare_string(self):
+        lit = self.primitives.Literal(value='Foo', datatype=self.profile.resolve('xsd:string'))
+        name = self.turtle_representation(node = lit, profile = self.profile, name_map = None, bnode_name_maker = None)
+        self.assertEqual(name, '"Foo"')
+    
+    def test_language_string(self):
+        lit = self.primitives.Literal(value='Foo', language="en")
+        name = self.turtle_representation(node = lit, profile = self.profile, name_map = None, bnode_name_maker = None)
+        self.assertEqual(name, '"Foo"@en')
+    
+    def test_random_datatype_bare_url(self):
+        lit = self.primitives.Literal(value='Foo', datatype=self.primitives.NamedNode("http://example.com/garply"))
+        name = self.turtle_representation(node = lit, profile = self.profile, name_map = None, bnode_name_maker = None)
+        self.assertEqual(name, '"Foo"^<http://example.com/garply>')
+    
+    def test_random_datatype_prefixed(self):
+        self.profile.setPrefix('ex', self.primitives.NamedNode('http://example.com/'))
+        lit = self.primitives.Literal(value='Foo', datatype=self.primitives.NamedNode("http://example.com/garply"))
+        name = self.turtle_representation(node = lit, profile = self.profile, name_map = None, bnode_name_maker = None)
+        self.assertEqual(name, '"Foo"^ex:garply')
+    
+    def test_named_node_bare(self):
+        node = self.primitives.NamedNode('http://example.com/foo')
+        name = self.turtle_representation(node = node, profile = self.profile, name_map = None, bnode_name_maker = None)
+        self.assertEqual(name, '<http://example.com/foo>')
+    
+    def test_named_node_prefixed(self):
+        self.profile.setPrefix('ex', self.primitives.NamedNode('http://example.com/'))
+        node = self.primitives.NamedNode('http://example.com/foo')
+        name = self.turtle_representation(node = node, profile = self.profile, name_map = None, bnode_name_maker = None)
+        self.assertEqual(name, 'ex:foo')
+
+class TestTurtleSerializer(TestCase):
+    def setUp(self):
+        from pymantic.parsers import turtle_parser
+        self.turtle_parser = turtle_parser
+    
+    def testSimpleSerialization(self):
+        basic_turtle = """@prefix dc: <http://purl.org/dc/terms/> .
+        @prefix example: <http://example.com/> .
+
+        example:foo dc:title "Foo" .
+        example:bar dc:title "Bar" .
+        example:baz dc:subject example:foo ."""
+
+        graph = self.turtle_parser.parse(basic_turtle)
